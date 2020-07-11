434b8311224a6fa26f5f4b41c2cb7ceeb860a7b0
Renato Cadecaro <rcadecaro@gmail.com>
2020-07-11 16:53:02 -0300

    chore: GET /transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -22,15 +22,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(Type.INCOME);
+    const outcome = this.calculateBalance(Type.OUTCOME);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
     const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if (type === Type.OUTCOME && total < value) {
+      throw Error('Cannot create outcome transaction without a valid balance');
+    }
 
     this.transactions.push(transaction);
 
